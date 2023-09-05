ca5d8937908c7c14e9f2f43ca610156f9476e016
Nikita Zhavoronkov <nikzh@nikzh.com>
2023-09-04 21:04:25 -0400

    Tea. Earl Grey. Hot.

---
+++ README.md
@@ -0,0 +1,30 @@
+Awesome Block Explorers
+=======================
+
+What's this list?
+-----------------
+
+One of the main goals of crypto is decentralization, and we want this to be applicable to block explorers as well!
+
+Wouldn't be it useful for a block explorer user to be able to cross-check their transaction on another explorer?
+We thought it would, so we've decided to come up with this list of good explorers for multiple chains.
+Not only it is good for decentralization, but it also allows users to find some extra details on other explorers.
+
+We currently integrate this list on [3xpl.com](3xpl.com) (see [example transaction](https://3xpl.com/bitcoin/transaction/4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b)).
+
+How to add my favourite explorer?
+---------------------------------
+
+New explorers should be added to the `Explorers/` folder in JSON format. Just follow the existing examples.
+
+Note that all blockchains should be present in `Chains/`. If your explorer provides data for a chain we don't yet support, please add a chain JSON file as well.
+`foreign_ids` are coin ids (URL slugs) on the following websites: CoinGecko ([example](https://www.coingecko.com/en/coins/bitcoin)), CoinMarketCap ([example](https://coinmarketcap.com/currencies/bitcoin/)), and Binance ([example](https://www.binance.com/en/price/bitcoin)). Don't forget to add examples!
+
+License
+-------
+
+This list is released under the terms of the MIT license.
+See [LICENSE.md](LICENSE.md) for more information.
+
+By contributing to this repository, you agree to license your work under the MIT license.
+Any work contributed where you are not the original author must contain its license header with the original author and source.
---
+++ Utils/Checker.php
@@ -0,0 +1,179 @@
+<?php declare(strict_types = 1);
+
+/*  This is a simple script to check whether the explorers are set up correctly and respond with code 200.
+ *  Tested on PHP 8.2 with php8.2-curl enabled. Distributed under the MIT software license, see LICENSE.md.  */
+
+$explorers = scandir('../Explorers');
+$chains = scandir('../Chains');
+$lib = [];
+
+// First, we check the validity of chain JSON files
+
+echo "Checking chain JSONs...\n";
+
+for ($i = 2, $c = count($chains); $i < $c; $i++)
+{
+    $t = str_pad($chains[$i] . '...', 23);
+    echo "\tChecking {$t}";
+
+    try
+    {
+        $json = json_decode(file_get_contents('../Chains/' . $chains[$i]), associative: true, flags: JSON_THROW_ON_ERROR);
+
+        if (!isset($json['id']))
+            throw new Error('`id` is not set');
+        if (!isset($json['name']))
+            throw new Error('`name` is not set');
+        if (!isset($json['foreign_ids']['coingecko']))
+            throw new Error('`foreign_ids.coingecko` is not set');
+        if (!isset($json['foreign_ids']['coinmarketcap']))
+            throw new Error('`foreign_ids.coinmarketcap` is not set');
+        if (!isset($json['foreign_ids']['binance']))
+            throw new Error('`foreign_ids.binance` is not set');
+        if (!isset($json['examples']['block']))
+            throw new Error('`examples.block` is not set');
+        if (!isset($json['examples']['transaction']))
+            throw new Error('`examples.transaction` is not set');
+        if (!isset($json['examples']['address']))
+            throw new Error('`examples.address` is not set');
+
+        $lib[($json['id'])] = $json;
+
+        echo "✅\n";
+    }
+    catch (Throwable $t)
+    {
+        echo "❌ Error: {$t->getMessage()}\n";
+    }
+}
+
+// Second, we check the validity of explorer json files
+
+echo "Checking explorer JSONs...\n";
+
+for ($i = 2, $c = count($explorers); $i < $c; $i++)
+{
+    $t = str_pad($explorers[$i] . '...', 23);
+    echo "\tChecking {$t}";
+
+    try
+    {
+        $json = json_decode(file_get_contents('../Explorers/' . $explorers[$i]), associative: true, flags: JSON_THROW_ON_ERROR);
+
+        if (!isset($json['id']))
+            throw new Error('`id` is not set');
+        if (!isset($json['name']))
+            throw new Error('`name` is not set');
+        if (!isset($json['description']))
+            throw new Error('`description` is not set');
+        if (!isset($json['link']))
+            throw new Error('`link` is not set');
+        if (!key_exists('x', $json))
+            throw new Error('`x` is not set (can be null)');
+        if (!key_exists('discord', $json))
+            throw new Error('`discord` is not set (can be null)');
+        if (!isset($json['blockchains']))
+            throw new Error('`blockchains` is not set');
+
+        foreach ($json['blockchains'] as $id => $blockchain)
+        {
+            if (!isset($lib[$id]))
+                throw new Error("`{$id}` is not a known chain");
+            if (!isset($blockchain['homepage']))
+                throw new Error("`homepage` is not set for {$id}");
+            if (!key_exists('block', $blockchain))
+                throw new Error("`block` is not set for {$id} (can be null)");
+            if (!key_exists('transaction', $blockchain))
+                throw new Error("`transaction` is not set for {$id} (can be null)");
+            if (!key_exists('address', $blockchain))
+                throw new Error("`address` is not set for {$id} (can be null)");
+            if (!key_exists('search', $blockchain))
+                throw new Error("`search` is not set for {$id} (can be null)");
+            if (isset($blockchain['block']) && !str_contains($blockchain['block'], '%block%'))
+                throw new Error("`block` should contain `%block%` for {$id}");
+            if (isset($blockchain['transaction']) && !str_contains($blockchain['transaction'], '%transaction%'))
+                throw new Error("`transaction` should contain `%transaction%` for {$id}");
+            if (isset($blockchain['address']) && !str_contains($blockchain['address'], '%address%'))
+                throw new Error("`address` should contain `%address%` for {$id}");
+            if (isset($blockchain['search']) && !str_contains($blockchain['search'], '%search%'))
+                throw new Error("`search` should contain `%search%` for {$id}");
+        }
+
+        echo "✅\n";
+    }
+    catch (Throwable $t)
+    {
+        echo "❌ Error: {$t->getMessage()}\n";
+    }
+}
+
+// Last, we check whether the explorers respond with 200
+
+$curl = curl_init();
+curl_setopt($curl, CURLOPT_NOBODY, true);
+curl_setopt($curl, CURLOPT_TIMEOUT, 5);
+
+function check_page($url)
+{
+    global $curl;
+
+    try
+    {
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_exec($curl);
+        $info = curl_getinfo($curl);
+
+        if ($info['http_code'] !== 200)
+            echo "⚠️ Code: {$info['http_code']}\n";
+        else
+            echo "✅ " . number_format($info['total_time'], decimals: 2). " s. \n";
+    }
+    catch (Throwable $t)
+    {
+        echo "⚠️ Error: {$t->getMessage()}\n";
+    }
+}
+
+echo "Checking explorers...\n";
+
+for ($i = 2, $c = count($explorers); $i < $c; $i++)
+{
+    $explorer = json_decode(file_get_contents('../Explorers/' . $explorers[$i]), true);
+
+    echo "\tChecking {$explorer['name']}...\n";
+
+    foreach ($explorer['blockchains'] as $j => $chain)
+    {
+        if (!isset($lib[$j]))
+        {
+            echo "\t\tThis chain is not present in the library ❌\n";
+            continue;
+        }
+
+        echo "\t\tChecking {$lib[$j]['name']}...\n";
+
+        if (!is_null($chain['homepage']))
+        {
+            echo "\t\t\tChecking homepage...    ";
+            check_page($chain['homepage']);
+        }
+
+        if (!is_null($chain['block']))
+        {
+            echo "\t\t\tChecking block...       ";
+            check_page(str_replace('%block%', $lib[$j]['examples']['block'], $chain['block']));
+        }
+
+        if (!is_null($chain['transaction']))
+        {
+            echo "\t\t\tChecking transaction... ";
+            check_page(str_replace('%transaction%', $lib[$j]['examples']['transaction'], $chain['transaction']));
+        }
+
+        if (!is_null($chain['address']))
+        {
+            echo "\t\t\tChecking address...     ";
+            check_page(str_replace('%address%', $lib[$j]['examples']['address'], $chain['address']));
+        }
+    }
+}
